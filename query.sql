18cec7afe3aa0e2649af8ab7b3144e89dfa443d3
Zaki Maliki <malikizakitop@gmail.com>
2022-08-12 15:05:04 +0700

    update

--- query.sql
+++ query.sql
@@ -3,26 +3,48 @@ CREATE DATABASE PRODUCT;
 CREATE TABLE product(
     id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
-    stoct INT NOT NULL,
+    stock INT NOT NULL,
     price INT NOT NULL,
-    category_id integer REFERENCES category (id)
+    category_id INT REFERENCES category (id),
+    transaksi_id INT REFERENCES transaksi (id)
 );
-
 CREATE TABLE category(
     id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL
+);
+
+CREATE TABLE transaksi(
+    id SERIAL PRIMARY KEY,
+    adress VARCHAR NOT NULL,
+    transaksi_detail_id INT REFERENCES transaksi_detail (id)
+);
+
+CREATE TABLE transaksi_detail(
+    id SERIAL PRIMARY KEY,
+    total INT NOT NULL,
+    payment_id INT REFERENCES payment(id)
+);
+
+CREATE TABLE payment(
+    id SERIAL PRIMARY KEY,
+    amount INT NOT NULL
 );
 
 DROP TABLE product CASCADE;
 
 SELECT * FROM category;
 
-SELECT * FROM category WHERE id=13;
+SELECT * FROM category WHERE id=1;
 
-INSERT INTO category(id,name) VALUES(13,’kursi’);
+SELECT product.name,product.stock,product.price,category.name as category
+FROM product
+INNER JOIN category
+ON product.category_id = category.id;
+
+INSERT INTO category(id,name) VALUES(1,’kursi’);
 
 INSERT INTO category(name) VALUES(’kursi’);
 
-UPDATE category SET name ='makanan' WHERE id=13;
+UPDATE category SET name ='furniture' WHERE id=1;
 
-DELETE FROM category WHERE id=13;
+DELETE FROM category WHERE id=1;
